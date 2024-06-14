626359246cf5a6c0f13be65bc5ce872f54d39caa
ITDevStack <163896368+ITDevStack@users.noreply.github.com>
2024-06-14 16:57:43 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -192,8 +192,8 @@ def menu_filtros(cars_historico):
 
         st.write(cars_historico)
 
-        dynamic_filters = DynamicFilters(cars_historico, filters=['Marca','Cilindrada', 'Estado','Transmision','MarcaModelo','Combustible', 'Color exterior','Placa','Estilo','Pasajeros', 'Color interior','Puertas'])
-        dynamic_filters.display_filters()
+        dynamic_filters = DynamicFilters(cars_historico, filters=['Marca','Cilindrada', 'Estado','Trasnmision','MarcaModelo','Combustible', 'Color exterior','Placa','Estilo','Pasajeros', 'Color interior','Puertas'])
+        dynamic_filters.display_filters(location='columns', num_columns=2)
 
 
 
