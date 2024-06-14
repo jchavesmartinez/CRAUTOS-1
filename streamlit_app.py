67a9a10908d3014c98c037cf3d7560f800745e5e
ITDevStack <163896368+ITDevStack@users.noreply.github.com>
2024-06-14 14:45:17 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,7 +189,7 @@ def menu_filtros(cars_historico):
     
     with st.expander("Menu de filtros"):
 
-        dynamic_filters = DynamicFilters(cars_historico, filters=['Marca','Estilo'])
+        dynamic_filters = DynamicFilters(cars_historico, filters=['Estilo','Marca'])
         dynamic_filters.display_filters(location='columns', num_columns=2)
 
         cars_historico=dynamic_filters.filter_df()
