9dc161798d11064c9f805572663945131c91ad85
ITDevStack <163896368+ITDevStack@users.noreply.github.com>
2024-06-15 13:42:33 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -772,9 +772,9 @@ def estadisticas_visuales(cars_historico):
 
         #columns_to_drop = [1, 3]  # Columns 'B' and 'D' by index
         modelo = modelo.drop(modelo.columns[columns_to_count_indices], axis=1)
-        modelo = modelo.drop(columns=['Color ext', 'Color int','Puertas','Libre impuestos','Negociable','Recibe','Provincia','Traspaso','Vehiculo_ID','Fecha ingreso','Visualizaciones','MarcaModelo_modelo_completo','Moneda','Marca_modelo_completo',
-                                        'Extraccion Dia','Grupo de años_modelo_completo','grupo_id','Visuales por Dia','Año_mean','Grupo de años_modelo','Estado','KM_mean','Precio_mean','Precio_std','precio_margen_mean','precio_margen_median','Precio_relativestd',
-                                        'km_margen_mean','km_margen_median','precio_margen_mean%','precio_margen_median%'])
+        #modelo = modelo.drop(columns=['Color ext', 'Color int','Puertas','Libre impuestos','Negociable','Recibe','Provincia','Traspaso','Vehiculo_ID','Fecha ingreso','Visualizaciones','MarcaModelo_modelo_completo','Moneda','Marca_modelo_completo',
+        #                                'Extraccion Dia','Grupo de años_modelo_completo','grupo_id','Visuales por Dia','Año_mean','Grupo de años_modelo','Estado','KM_mean','Precio_mean','Precio_std','precio_margen_mean','precio_margen_median','Precio_relativestd',
+        #                                'km_margen_mean','km_margen_median','precio_margen_mean%','precio_margen_median%'])
 
         modelo = modelo[['Marca_modelo', 'MarcaModelo_modelo','Combustible','Transmision','Estilo','Cilindrada','Año', 'Precio','Precio_median','Kilometraje','KM_median','Precio_count','nota_final','factor_marca','factor_precio','factor_año','factor_km','factor_extras','Comentarios','Pagina Web']]
         st.dataframe(modelo)
