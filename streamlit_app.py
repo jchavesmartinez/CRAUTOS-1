2ea37517a6b2032acd2152999113742aa24d827d
ITDevStack <163896368+ITDevStack@users.noreply.github.com>
2024-06-15 14:25:57 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -775,7 +775,7 @@ def estadisticas_visuales(cars_historico):
         #                                'km_margen_mean','km_margen_median','precio_margen_mean%','precio_margen_median%'])
 
         st.dataframe(modelo)
-        modelo = modelo[['URL']]
+        modelo = modelo[['URL', 'Marca_modelo_completo','MarcaModelo_modelo_completo','Año','Precio', 'Kilometraje']]
         st.dataframe(modelo)
         
 
